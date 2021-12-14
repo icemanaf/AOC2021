0484c933701ff33702c623846bd76481b408a1e5
icemanaf <dilantha.ariyapala@googlemail.com>
2021-12-14 08:38:01 +0000

    started Day 13

---
+++ AOC2021.Tests/Day 13/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC2021.Tests.Day_13
+{
+    [TestFixture]
+    public class Tests
+    {
+        private String<Enumerable> GetTestData()
+        {
+
+        }
+    }
+}
---
+++ AOC2021/Day13/Day13.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC2021.Day13
+{
+    class Day13
+    {
+    }
+}
